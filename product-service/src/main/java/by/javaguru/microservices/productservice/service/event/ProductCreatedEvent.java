c77def62e8ef02fa1e86dd32d338a57335a3bd36
Michael_Poedinenko <michael.poedinenko.mxr@gmail.com>
2024-10-08 14:02:56 +0500

    3 sprint fix

--- product-service/src/main/java/by/javaguru/microservices/productservice/service/event/ProductCreatedEvent.java
+++ product-service/src/main/java/by/javaguru/microservices/productservice/service/event/ProductCreatedEvent.java
@@ -1,14 +1,20 @@
 package by.javaguru.microservices.productservice.service.event;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.math.BigDecimal;
 
 public class ProductCreatedEvent {
+    @JsonProperty("productId")
     private String productId;
 
+    @JsonProperty("title")
     private String title;
 
+    @JsonProperty("price")
     private BigDecimal price;
 
+    @JsonProperty("quantity")
     private Integer quantity;
 
     public ProductCreatedEvent(String productId, String title, BigDecimal price, Integer quantity) {
